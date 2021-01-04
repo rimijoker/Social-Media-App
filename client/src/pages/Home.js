5764bd656ceb28a74cd061e4e240a0713857d3e1
rimijoker <anz19999999999@gmail.com>
2021-01-05 02:01:27 +0530

    fixed bug of Apollo Client blowing up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,13 +6,12 @@ import { Grid } from "semantic-ui-react";
 import PostCard from "../components/PostCard";
 
 function Home() {
-    const {
-        loading,
-        data: { getPosts: posts },
-    } = useQuery(FETCH_POSTS_QUERY);
+    const { loading, data: { getPosts: posts } = {} } = useQuery(
+        FETCH_POSTS_QUERY
+    );
 
     return (
-        <Grid columns={3} doubling="true" stackable="true">
+        <Grid columns={3} doubling={true} stackable={true}>
             <Grid.Row className="page-title">
                 <h1>Recent Posts</h1>
             </Grid.Row>
